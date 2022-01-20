54ec97d143fe979eeb63c51e2c5967f0ecaeae60
Dasha-290110 <77583048+Dasha-290110@users.noreply.github.com>
2022-01-20 16:06:56 +0530

    Add files via upload

---
+++ living room.js
@@ -0,0 +1,33 @@
+status="";
+img="";
+objects=[];
+
+
+ function preload(){
+     img=loadImage('living room.jpg');
+
+ }
+
+ function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectdetector=ml5.objectDetector('cocossd',modalLoaded);
+document.getElementById('status').innerHTML="Status : Detecting Objects";
+}
+
+function modalLoaded(){
+console.log("Modal is loaded") ;
+status=true;
+objectdetector.detect(img,gotresult);
+}
+
+function gotresult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+    }
+function draw(){
+    image(img,0,0,640,420);
+}
